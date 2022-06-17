7e21b9a650e787931e9a17bc7f7aedff9500e612
Luca Ghislotti <ghislottiluca@gmail.com>
2022-06-17 09:35:10 +0200

    added all gain/pedestal plots

--- gain_pedestal_analysis.m
+++ gain_pedestal_analysis.m
@@ -9,7 +9,7 @@ f = figure('visible','on');
 % HIGH GAIN (low energy, 10-100 keV) -> (1:11)
 % LOW GAIN (high energy, 40-55 MeV) -> (47:end)
 x = X_keV(47:end);
-y = T_tab_30A_mean(47:end); % manually change temperature to get gain
+y = T_tab_30B_mean(47:end); % manually change temperature to get gain
 plot(x,y,'LineWidth',2)
 
 % Fit line to data using polyfit
@@ -47,8 +47,10 @@ x = [-40 -38 -36 -34 -32 -30 -20 -10 0 10 20 30] % [all steps]
 
 %y = [0.87769 0.89379 0.90745 0.91659 0.91969 0.93293 0.98319 1.0447 1.0907 1.1466 1.1954 1.2323] % HIGH GAIN (low energy) [all steps, auto, 0111]
 %y = [0.01121 0.011302 0.011356 0.011395 0.011451 0.011526 0.011805 0.012039 0.012265 0.012552 0.012746 0.012512]; % LOW GAIN (high energy) [all steps, auto, 0111]
-y = [0.9286 0.9477 0.9623 0.96948 0.98281 0.99537 1.0465 1.0977 1.1545 1.2153 1.2572 1.2898] % HIGH GAIN (low energy) [all steps, auto, 0011]
+%y = [0.9286 0.9477 0.9623 0.96948 0.98281 0.99537 1.0465 1.0977 1.1545 1.2153 1.2572 1.2898] % HIGH GAIN (low energy) [all steps, auto, 0011]
 %y = [0.011201 0.011272 0.011345 0.011421 0.011454 0.011490 0.01174 0.011989 0.012247 0.012492 0.012473 0.011799] % LOW GAIN (high energy) [all steps, auto, 0011]
+%y = [0.85593 0.84394 0.82741 0.79938 0.78246 0.75099 0.63282 0.51795 0.39415 0.29776 0.21870 0.16317]; % HIGH HAIN (low energy) [all steps, 530mv]
+y = [0.011237 0.011315 0.011399 0.011426 0.011480 0.011527 0.011846 0.012168 0.012511 0.012850 0.013139 0.013459]; % LOW GAIN (high energy) [all steps, 530mv]
 
 plot(x,y, '-o')
 
@@ -65,18 +67,18 @@ y_est = polyval(c,x);
 hold on
 plot(x,y_est,'r--','LineWidth',1.5)
 
-title({'Gain in X-ray detection region (40-100 keV)', 'CSAVrefGM: auto [HRRR = (0011)_2]'});
+%title({'Gain in X-ray detection region (40-100 keV)', 'CSAVrefGM: auto [HRRR = (0011)_2]'});
 %title({'Gain in Muon detection region (40-55 MeV)', 'CSAVrefGM: auto [HRRR = (0011)_2]'});
 %title({'Gain in X-ray detection region (40-100 keV)', 'CSAVrefGM: auto [HRRR = (0111)_2]'});
 %title({'Gain in Muon detection region (40-55 MeV)', 'CSAVrefGM: auto [HRRR = (0111)_2]'});
 %title({'Gain in X-ray detection region (10-100 keV)', 'CSAVrefGM: 530mV'});
-%title({'Gain in Muon detection region (40-55 MeV)', 'CSAVrefGM: 530mV'});
+title({'Gain in Muon detection region (40-55 MeV)', 'CSAVrefGM: 530mV'});
 
 xlabel('Temperature [°C]');
 ylabel('Linear Gain [ADU/keV]');
 legend('Mean gain at temperature x', ['y = ' num2str(c(2)) ' + ' num2str(c(1)) ' * x'], 'Location','southeast')
-savefig('fig/low_energy_gain_auto_0011.fig')
-exportgraphics(gcf, 'pdf/low_energy_gain_auto_0011.pdf','ContentType','vector');
+savefig('fig/high_energy_gain_530mv.fig')
+exportgraphics(gcf, 'pdf/high_energy_gain_530mv.pdf','ContentType','vector');
 hold off
 
 
@@ -84,8 +86,8 @@ hold off
 
 clc;
 % manually change temperature to get pedestal
-mean_ped_low_energy = mean(T_tab_30A_mean(1:11));
-mean_ped_high_energy = mean(T_tab_30A_mean(47:end))
+mean_ped_low_energy = mean(T_tab_30B_mean(1:11));
+mean_ped_high_energy = mean(T_tab_30B_mean(47:end))
 
 
 %% PLOT PEDESTAL RESULTS
@@ -108,8 +110,9 @@ x = [-40 -38 -36 -34 -32 -30 -20 -10 0 10 20 30] % [all steps]
 %y = [147.8092 149.1418 151.5099 151.9342 152.846 154.8000 162.0905 168.7897 175.8611 181.6455 188.5995 194.1047]; % HIGH GAIN (low energy) [all steps, auto, 0111]
 %y = [1418.2 1433.0 1452.7 1459.0 1467.6 1486.2 1556.0 1625.0 1692.7 1760.3 1826.1 1881.9]; % LOW GAIN (high energy) [all steps, auto, 0111]
 %y = [150.4564 153.0806 154.6233 155.9029 157.5707 157.9231 164.8835 171.6629 178.3843 185.6536 192.7818 190.9261]; % HIGH GAIN (low energy) [all steps, auto, 0011]
-y = [1448.4 1466.3 1483.2 1495.5 1509.7 1518.7 1589.9 1659.7 1728.9 1798.2 1862.5 1913.3]; % LOW GAIN (high energy) [all steps, auto, 0011]
-
+%y = [1448.4 1466.3 1483.2 1495.5 1509.7 1518.7 1589.9 1659.7 1728.9 1798.2 1862.5 1913.3]; % LOW GAIN (high energy) [all steps, auto, 0011]
+%y = [145.7534 146.4100 145.6180 145.3372 144.1384 142.4282 137.1384 131.2691 124.3402 119.8640 117.3576 117.1489]; % HIGH GAIN (low energy) [all steps, 530mV]
+y = [1404.5 1406.1 1401.3 1397.4 1398.3 1384.5 1361.7 1341.0 1310.3 1285.1 1262.8 1248.1]; % LOW GAIN (high energy) [all steps, 530mV]
 
 plot(x,y, '-o')
 
@@ -129,13 +132,13 @@ plot(x,y_est,'r--','LineWidth',2)
 %title({'Pedestal in X-ray detection region (10-100 keV)', 'CSAVrefGM: auto [HRRR = (0111)_2]'});
 %title({'Pedestal in Muon detection region (40-55 MeV)', 'CSAVrefGM: auto [HRRR = (0111)_2]'});
 %title({'Pedestal in X-ray detection region (10-100 keV)', 'CSAVrefGM: auto [HRRR = (0011)_2]'});
-title({'Pedestal in Muon detection region (40-55 MeV)', 'CSAVrefGM: auto [HRRR = (0011)_2]'});
+%title({'Pedestal in Muon detection region (40-55 MeV)', 'CSAVrefGM: auto [HRRR = (0011)_2]'});
 %title({'Pedestal in X-ray detection region (10-100 keV)', 'CSAVrefGM: 530mV'});
-%title({'Pedestal in Muon detection region (40-55 MeV)', 'CSAVrefGM: 530mV'});
+title({'Pedestal in Muon detection region (40-55 MeV)', 'CSAVrefGM: 530mV'});
 
 xlabel('Temperature [°C]');
 ylabel('Linear Gain [ADU/keV]');
-legend('Mean pedestal at temperature x', ['y = ' num2str(c(2)) ' + ' num2str(c(1)) ' * x'], 'Location','southeast')
-savefig('fig/high_energy_pedestal_auto_0011.fig')
-exportgraphics(gcf, 'pdf/high_energy_pedestal_auto_0011.pdf','ContentType','vector');
+legend('Mean pedestal at temperature x', ['y = ' num2str(c(2)) ' + ' num2str(c(1)) ' * x'], 'Location','northeast')
+savefig('fig/high_energy_pedestal_530mv.fig')
+exportgraphics(gcf, 'pdf/high_energy_pedestal_530mv.pdf','ContentType','vector');
 hold off
